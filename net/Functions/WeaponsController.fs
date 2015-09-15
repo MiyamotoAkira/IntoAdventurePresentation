8bb8c00e575a19dc1b9f13a683d2afeec9dcc21b
MiyamotoAkira <yefo.akira@gmail.com>
2015-09-15 21:57:39 +0100

    WeaponsController with injection working

--- net/Functions/WeaponsController.fs
+++ net/Functions/WeaponsController.fs
@@ -4,11 +4,11 @@ open System
 open System.Web.Http
 open WeaponRepository
 
-type WeaponsController () = 
+type WeaponsController (getSingleWeapon : int -> Weapon) = 
     inherit ApiController()
 
     [<Route ("weapons")>]
     member this.getAllWeapons () = WeaponRepository.getAllWeapons ()
 
     [<Route ("weapons/{weaponid}/getrandomdamage")>]
-    member this.getRandomDamageForWeapon weaponId = WeaponRepository.getWeapon weaponId |> Calculations.getRandomDamage
+    member this.getRandomDamageForWeapon weaponId = getSingleWeapon weaponId |> Calculations.getRandomDamage
