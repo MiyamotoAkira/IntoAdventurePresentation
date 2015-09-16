8be636315a591d9ada2229420fff2afd2c4634fe
MiyamotoAkira <yefo.akira@gmail.com>
2015-09-16 08:39:03 +0100

    Added the pattern matching solution

---
+++ PatternMatching/CSharpSample/Yathzee.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PatternMatching
+{
+    public enum Options
+    {
+        ONES = 1,
+        TWOS,
+        THREES,
+        FOURS,
+        FIVES,
+        SIXES,
+        PAIR,
+        TWOPAIR,
+        THREEKIND,
+        FOURKIND,
+        SMALL,
+        LARGE,
+        FULL,
+        YAHTZEE
+    }
+
+    public class Yahtzee
+    {
+        public int CalculateRoll(IEnumerable<int> dice, Options option)
+        {
+            switch (option)
+            {
+            case Options.ONES:
+            case Options.TWOS:
+            case Options.THREES:
+            case Options.FOURS:
+            case Options.FIVES:
+            case Options.SIXES:
+                return CalculateSimpleNumbers(dice, (int)option);
+            case Options.PAIR:
+                return CalculateKind(dice, 2);
+            case Options.THREEKIND:
+                return CalculateKind(dice, 3);
+            case Options.FOURKIND:
+                return CalculateKind(dice, 4);
+            case Options.TWOPAIR:
+                return CalculateDoubleKind(dice);
+            case Options.FULL:
+                return CalculateFullKind(dice);
+            case Options.SMALL:
+                return CalculateSmallStraight(dice);
+            case Options.LARGE:
+                return CalculateLargeStraight(dice);
+            case Options.YAHTZEE:
+                return CalculateYahtzee(dice);
+            }
+
+            return 0;
+        }
+
+        public int CalculateYahtzee(IEnumerable<int> dice)
+        {
+            var consolidated = ConsolidateDice(dice);
+            if (consolidated.Count == 1)
+            {
+                return 50;
+            }
+
+            return 0;
+        }
+
+        public int CalculateSmallStraight(IEnumerable<int> dice)
+        {
+            if (CalculateStraight(dice, 0))
+            {
+                return 15;
+            }
+
+            return 0;
+        }
+
+        public int CalculateLargeStraight(IEnumerable<int> dice)
+        {
+            if (CalculateStraight(dice, 1))
+            {
+                return 20;
+            }
+
+            return 0;
+        }
+
+        public bool CalculateStraight(IEnumerable<int> dice, int modifier)
+        {
+            var consolidated = ConsolidateDice(dice);
+            var ordered = consolidated.OrderBy(x => x.Key);
+            if (ordered.Count() == 5 && ordered.First().Key == (1 + modifier) && ordered.Last().Key == (5 + modifier))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int CalculateFullKind(IEnumerable<int> dice)
+        {
+            var consolidated = ConsolidateDice(dice);
+            var triple = consolidated.FirstOrDefault(x => x.Value == 3);
+            var pair = consolidated.FirstOrDefault(x => x.Value == 2);
+            if (pair.Key > 0 && triple.Key > 0)
+            {
+                return triple.Key * 3 + pair.Key * 2;
+            }
+
+            return 0;
+        }
+
+        public int CalculateDoubleKind(IEnumerable<int> dice)
+        {
+            var consolidated = ConsolidateDice(dice);
+            var ordered = consolidated.Where(x => x.Value >= 2).OrderByDescending(x => x.Key);
+
+            if (ordered.Count() > 1)
+            {
+                return (ordered.First().Key * 2) + (ordered.Skip(1).First().Key * 2);
+            }
+
+            return 0;
+        }
+
+        public int CalculateKind(IEnumerable<int>dice, int size)
+        {
+            var consolidated = ConsolidateDice(dice);
+            var ordered = consolidated.Where(x => x.Value >= size).OrderByDescending(x => x.Key);
+            if (ordered.Any())
+            {
+                var biggerNumber = ordered.First();
+                return biggerNumber.Key * size;
+            }
+
+            return 0;
+        }
+
+        public int CalculateSimpleNumbers(IEnumerable<int> dice, int die)
+        {
+            var consolidated = ConsolidateDice(dice);
+            if (consolidated.ContainsKey(die))
+            {
+                return die * consolidated[die];
+            }
+
+            return 0;
+        }
+
+        public Dictionary<int, int> ConsolidateDice(IEnumerable<int> dice)
+        {
+            Dictionary<int, int> consolidated = new Dictionary<int, int>();
+
+            foreach (int die in dice)
+            {
+                if (!consolidated.ContainsKey(die))
+                {
+                    consolidated.Add(die, 0);
+                }
+
+                consolidated[die] += 1;
+            }
+
+            return consolidated;
+        }
+    }
+}
+
---
+++ PatternMatching/fsharpSample/Yathzee.fs
@@ -0,0 +1,106 @@
+﻿namespace PatternMatching
+
+module Yathzee =
+    type Category = 
+        |Ones
+        |Twos
+        |Threes
+        |Fours
+        |Fives
+        |Sixes
+        |Pair 
+        |TwoPair 
+        |ThreeOfAKind 
+        |FourOfAKind 
+        |SmallStraight 
+        |LargeStraight 
+        |FullHouse 
+        |Yahtzee
+        |Chance
+
+    let values = [1; 2; 3; 4; 5; 6;]
+
+    let collect minimumRepetitions dice =
+        values 
+        |> List.map (fun x -> (x, (dice |> List.filter (fun y -> y = x) |> List.length))) 
+        |> List.filter ( fun (_,y) -> y >= minimumRepetitions)
+
+    let calculatePair dice =
+        let atLeastPairs = dice |> collect 2
+        match atLeastPairs with
+        | head::_ ->
+            let value, _ = atLeastPairs |> List.reduce (fun max value -> 
+                                                        if fst value > fst max 
+                                                        then value
+                                                        else max)
+            value * 2
+        | _ -> 0
+    
+    let calculateMultiple multiple dice =
+        let multipleDice = dice |> collect multiple
+        match multipleDice with 
+        | (x, _)::[] -> x * multiple
+        | _ -> 0
+
+    let calculateTwoPair dice =
+        let atLeastPairs = dice |> collect 2
+        match atLeastPairs with
+        | (x, _)::(y,_)::[] -> x * 2 + y * 2
+        | _ ->   0
+
+    let calculateThree dice =
+        calculateMultiple 3 dice
+
+    let calculateFour dice =
+        calculateMultiple 4 dice
+
+    let isAStraightOfType straight dice =
+        dice |> List.sort |> List.zip straight |> List.forall (fun (v1,v2) -> v1 = v2)
+
+    let calculateStraight straight value dice = 
+        if dice |> isAStraightOfType straight
+        then value
+        else 0
+
+    let calculateSmall dice =
+        calculateStraight [1;2;3;4;5] 15 dice
+
+    let calculateLarge dice =
+        calculateStraight [2;3;4;5;6] 20 dice
+
+    let calculateFull dice =
+        let collected  = dice |> collect 2
+        match collected with
+        | (v1,y1) :: (v2,y2) ::[] when (y1, y2) = (2, 3) || (y1 ,y2) = (3, 2)-> (v1 * y1) + (v2 * y2)
+        | _ -> 0
+
+    let calculateYathzee dice =
+        let collected = dice |> collect 5
+        match collected with
+        | head::[] -> 50
+        | _ -> 0
+
+    let calculateChance dice =
+        dice |> List.sum
+
+    let calculateSingles dieValue dice =
+        dice |> List.filter (fun y -> y = dieValue) |> List.sum
+
+    let calculateScore category d1 d2 d3 d4 d5 =
+        let dice = [d1; d2; d3; d4; d5]
+        match category with
+        | Ones -> calculateSingles 1 dice
+        | Twos -> calculateSingles 2 dice
+        | Threes -> calculateSingles 3 dice
+        | Fours -> calculateSingles 4 dice
+        | Fives -> calculateSingles 5 dice
+        | Sixes -> calculateSingles 6 dice
+        | Pair -> calculatePair dice
+        | TwoPair -> calculateTwoPair dice
+        | ThreeOfAKind -> calculateThree dice
+        | FourOfAKind -> calculateFour dice
+        | SmallStraight -> calculateSmall dice
+        | LargeStraight -> calculateLarge dice
+        | FullHouse -> calculateFull dice
+        | Yahtzee -> calculateYathzee dice
+        | Chance -> calculateChance dice
